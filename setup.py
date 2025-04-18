c398b3f6a30bd735083641c108d59dbf8401e78b
Francois Primeau <fprimeau@uci.edu>
2025-04-18 14:54:42 -0700

    fixed a missing comma in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ESS230tools",
+    name="woatools",
     version="0.1",
     packages=find_packages(),
     install_requires=[
@@ -11,7 +11,7 @@ setup(
         'tqdm'
     ],
     author="Francois Primeau",
-    author_email="fprimeau@uci.edu"
+    author_email="fprimeau@uci.edu",
     description="ESS230 Tools for downloading and processing World Ocean Atlas 2023 data",
     keywords="oceanography, WOA, data analysis, UCI ESS230",
     url="https://github.com/fprimeau/woatools",
